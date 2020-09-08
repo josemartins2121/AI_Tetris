a9d11bf9c2cebf7bc73fd4cefe5a585d3d8e2d82
josemartins2121 <jose.martins20001@gmail.com>
2020-09-08 02:46:14 +0100

    added score and hard drop

--- game.py
+++ game.py
@@ -139,6 +139,15 @@ class piece:
         self.y += size_piece
         if not pos_valid(self,locked_positions):
             self.y -=  size_piece
+    def free_fall(self):
+        n=0
+        while 1:
+            self.y += size_piece
+            if not pos_valid(self,locked_positions):
+                self.y -= size_piece
+                break
+            n+=1
+        return n
     def change_orientation(self):
         previous = self.orientation
         self.orientation = (self.orientation + 1) % len(self.shape)
@@ -152,14 +161,12 @@ def get_piece():
 
 
 def print_shape(piece):
-
     for lines_index,lines in enumerate(piece.shape[piece.orientation]):
         for point_index,point in enumerate(lines):
             if point == '0':
                 pygame.draw.rect(background,piece.color, (piece.x+(point_index-2)*size_piece,piece.y+(lines_index-2)*size_piece,size_piece,size_piece))
 
 def pos_valid(piece,locked_positions):
-
     for lines_index,lines in enumerate(piece.shape[piece.orientation]):
         for point_index,point in enumerate(lines):
             if point == '0':
@@ -185,7 +192,8 @@ def fallen_piece(piece,locked_positions):
     return False
 
 def add_locked(piece,locked_positions):
-    for lines_index,lines in enumerate(piece.shape[piece.orientation]):
+    for lines_index,lines in enumerate(
+        piece.shape[piece.orientation]):
         for point_index,point in enumerate(lines):
             if point == '0':
                 curr_point_x = piece.x+(point_index-2)*size_piece
@@ -218,14 +226,37 @@ def clear_line(locked_positions,line,n_elem):
                 locked_positions[y][x]= (0,0,0)
                 locked_positions[y+1][x] = previous 
 
-
+def show_score(total_points,level):
+    scoretext = myfont.render(str(total_points), 1, (88, 105, 148))
+    leveltext = myfont.render(str(level),1,(88, 105, 148))
+    scorebox = scoretext.get_rect(center=(gap*2+play_width+75,gap+35))
+    levelbox = scoretext.get_rect(center=(gap*2+play_width+75,2*gap+70+35))
+    screen.blit(leveltext,levelbox)
+    screen.blit(scoretext, scorebox)
 
 atual_piece = get_piece()
 start_time = time()
+start_time_level = time()
+start_time_hard_drop = 0
 locked_positions = [[(0,0,0) for _ in range(10) ]for _ in range(20)]
 n_elem = [0 for _ in range (20)]
+total_points = 0
+myfont = pygame.font.SysFont("Arial",30)
+hard_drop_mode = False
+velocity = 0.5
 
+pontuation = [40,100,300,1200]
+
+def add_points_lines(n,level):
+    total=0
+    for j in reversed(range(1,5)):
+        total += int(n/j)*pontuation[j-1]*(level+1)
+        n = n % j
+    return total
+
+level = 0
 while 1:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -243,25 +274,37 @@ while 1:
                 if not pos_valid(atual_piece,locked_positions):
                     atual_piece.x += size_piece 
             if event.key == pygame.K_DOWN:
-                atual_piece.y += size_piece 
-                #print(pos_valid(atual_piece))
-                if not pos_valid(atual_piece,locked_positions):
-                    atual_piece.y -= size_piece 
+                total_points += atual_piece.free_fall() 
+                start_time_hard_drop = time()
+                hard_drop_mode = True
             if event.key == pygame.K_UP:
                 atual_piece.change_orientation()
 
     curr_time = time()
+    if velocity > 0.1:
+        velocity = 0.5+(level*(-0.02))
 
-    if curr_time - start_time > 0.5:
-        #print(curr_time-start_time)
+    if curr_time - start_time > velocity:
+        #print(curr_time-start_time) 
         start_time = time()
         atual_piece.fall()
-    
-    #print(fallen_piece(atual_piece,locked_positions))
 
-    if fallen_piece(atual_piece,locked_positions):
-        add_locked(atual_piece,locked_positions)
-        atual_piece = get_piece()
+    if curr_time - start_time_level > 15:
+        start_time_level = time()
+        level += 1
+
+    if curr_time - start_time_hard_drop > 0.25:
+        hard_drop_mode = False
+
+    print(hard_drop_mode)
+    #print(fallen_piece(atual_piece,locked_positions))
+    if not hard_drop_mode:
+
+        if fallen_piece(atual_piece,locked_positions):
+            add_locked(atual_piece,locked_positions)
+            atual_piece = get_piece()
+            if not pos_valid(atual_piece,locked_positions):
+                sys.exit()
     
 
     background.fill(black)
@@ -269,12 +312,16 @@ while 1:
     pygame.draw.rect(background, (blue_bg), (play_width+2*gap,gap,150,70))
     pygame.draw.rect(background, (blue_bg), (play_width+2*gap,2*gap+70,150,70))
     
-    
+    cleared_lines = 0
     while 1:
         line = check_lines(n_elem)
         if line == -1:
             break
         clear_line(locked_positions,line,n_elem)
+        cleared_lines += 1
+
+    if cleared_lines != 0:
+        total_points += add_points_lines(cleared_lines,level)
 
     #print(n_elem)
     print_shape(atual_piece)
@@ -285,7 +332,8 @@ while 1:
     print(atual_piece.shape.index)
     print(atual_piece.color)
     print(atual_piece.orientation)  """
-
+    #print(total_points)
     
     screen.blit(background,(0,0))
+    show_score(int(total_points),level)
     pygame.display.flip()
